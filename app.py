752c7bde78503862b2549f94315557ba1b71b4bc
Denis Kutukov <dskut@yandex-team.ru>
2015-09-25 21:20:02 +0300

    add more fake bots

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import json
 from flask import Flask, Response
 app = Flask(__name__)
 
@@ -7,10 +8,18 @@ app = Flask(__name__)
 def index():
     return 'Hello Fenegram Bots'
 
+def create_bot(ind):
+    return {
+        "name": "fenegram_bot %s" % ind, 
+        "image": "http://fenegram-bot-store.herokuapp.com/icon%s.jpg" % ind,
+        "description": "Bot Description %s" % ind
+    }
+
 @app.route('/list')
 def list():
-    json = '{"items":[{"name": "fenegram_bot", "image": "http://fenegram-bot-store.herokuapp.com/icon.jpg", "description": "Fenerbahce Yandex bot"}]}'
-    return Response(json, mimetype='application/json')
+    bots = [create_bot(i) for i in xrange(1,21)]
+    resp_dict = {"items": bots}
+    return Response(json.dumps(resp_dict), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
