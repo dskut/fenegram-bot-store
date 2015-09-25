9dfd07c73e2f7a757375c659ad5dcc8543669ddb
Denis Kutukov <dskut@yandex-team.ru>
2015-09-25 21:06:14 +0300

    return json in bots

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python
 
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Hello Fenegram Bots'
+
+@app.route('/list')
+def list():
+    json = '{"items":[{"name": "fenegram_bot", "image": "http://fenegram-bot-store.herokuapp.com/icon.jpg", "description": "Fenerbahce Yandex bot"}]}'
+    return Response(json, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
