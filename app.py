7283722b3f6f324fdd7e47a0d1fc0b1b5a6519b8
Denis Kutukov <dskut@yandex-team.ru>
2015-09-26 13:03:05 +0300

    added chants

--- app.py
+++ app.py
@@ -5,10 +5,23 @@ import os.path
 from flask import Flask, Response, send_file, abort
 app = Flask(__name__)
 
+BOTS_COUNT=20
+CHANTS_COUNT=20
+BACKEND_URL="http://fenegram.herokuapp.com"
 
 @app.route('/')
 def index():
-    return 'Available methods:\n/ping\n/list'
+    return '''
+    Welcome to Fenegram backend!
+    <br/>
+    Available methods:
+    <br/>
+    <a href="/ping">/ping</a>
+    <br/>
+    <a href="/bots">/bots</a>
+    <br/>
+    <a href="/chants">/chants</a>
+    '''
 
 @app.route('/ping')
 def ping():
@@ -19,16 +32,31 @@ def create_bot(ind):
         "usernamename": "@fenegram_%s_bot" % ind, 
         "title": "Fenegram Bot %s" % ind, 
         "id": "bot_id_%s" % ind, 
-        "image": "http://fenegram-bot-store.herokuapp.com/static/icon%s.png" % ind,
+        "image": "%s/static/icon%s.png" % (BACKEND_URL, ind),
         "description": "Bot Description %s" % ind
     }
 
 def get_bots():
-    return [create_bot(i) for i in xrange(1,21)]
+    return [create_bot(i) for i in xrange(1, BOTS_COUNT+1)]
 
-@app.route('/list')
-def list():
+@app.route('/bots')
+def bots():
     resp_dict = {"items": get_bots()}
+    return Response(json.dumps(resp_dict), mimetype='application/json')
+
+def create_chant(ind):
+    return {
+        "title": "Fenerbahce chant %s" % ind,
+        "lyrics": "Lorem ipsum",
+        "url": "%s/static/chant%s.mp3" % (BACKEND_URL, ind)
+    }
+
+def get_chants():
+    return [create_chant(i) for i in xrange(1, CHANTS_COUNT+1)]
+
+@app.route('/chants')
+def chants():
+    resp_dict = {"items": get_chants()}
     return Response(json.dumps(resp_dict), mimetype='application/json')
 
 @app.route('/static/<string:filename>')
@@ -38,7 +66,6 @@ def static_file(filename):
         return send_file(path)
     else:
         abort(404)
-
 
 if __name__ == '__main__':
     app.run()
